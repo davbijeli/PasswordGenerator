ed754bfd97345a35ae493b5ac0505f164318c461
Davor Bijelić <davor.bijeli94@gmail.com>
2024-12-16 14:21:30 +0100

    Add files via upload

---
+++ CS_PasswordGenerator/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace CS_PasswordGenerator
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPasswordGenerator = new System.Windows.Forms.Label();
+            this.btnGeneratePassword = new System.Windows.Forms.Button();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPasswordGenerator
+            // 
+            this.lblPasswordGenerator.AutoSize = true;
+            this.lblPasswordGenerator.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPasswordGenerator.Location = new System.Drawing.Point(2, 9);
+            this.lblPasswordGenerator.Name = "lblPasswordGenerator";
+            this.lblPasswordGenerator.Padding = new System.Windows.Forms.Padding(10);
+            this.lblPasswordGenerator.Size = new System.Drawing.Size(399, 62);
+            this.lblPasswordGenerator.TabIndex = 0;
+            this.lblPasswordGenerator.Text = "Password Generator";
+            this.lblPasswordGenerator.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGeneratePassword
+            // 
+            this.btnGeneratePassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnGeneratePassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGeneratePassword.Location = new System.Drawing.Point(73, 125);
+            this.btnGeneratePassword.Name = "btnGeneratePassword";
+            this.btnGeneratePassword.Size = new System.Drawing.Size(245, 73);
+            this.btnGeneratePassword.TabIndex = 1;
+            this.btnGeneratePassword.Text = "GENERATE";
+            this.btnGeneratePassword.UseVisualStyleBackColor = false;
+            this.btnGeneratePassword.Click += new System.EventHandler(this.btnGeneratePassword_Click);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.BackColor = System.Drawing.Color.White;
+            this.lblPassword.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(73, 212);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(245, 41);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 354);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnGeneratePassword);
+            this.Controls.Add(this.lblPasswordGenerator);
+            this.Name = "FrmMain";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPasswordGenerator;
+        private System.Windows.Forms.Button btnGeneratePassword;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
+
---
+++ CS_PasswordGenerator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_PasswordGenerator
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private static string CreateRandomPassword(int length)
+        {
+            // Create a string of characters, numbers, and special characters that are allowed in the password
+            string validChars = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?_-";
+            Random random = new Random();
+
+            // Select one random character at a time from the string
+            // and create an array of chars
+            char[] chars = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(0, validChars.Length)];
+            }
+            return new string(chars);
+        }
+
+        private void btnGeneratePassword_Click(object sender, EventArgs e)
+        {
+            lblPassword.Text = CreateRandomPassword(8);
+        }
+    }
+}
